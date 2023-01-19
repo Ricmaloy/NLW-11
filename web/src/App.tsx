a11fdb04a6cfc9b3a7133691d0a7f6006e91dbdb
Ricardo Zamboni <ricardo-testando@hotmail.com>
2023-01-19 15:08:16 -0300

    complete day 01 :fire:

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from './components/Habit';
+
+import './styles/global.css';
+
+function App() {
+  return (
+    <>
+      <Habit completed={4} />
+      <p>Hello World</p>
+    </>
+  );
+}
+
+export default App;
